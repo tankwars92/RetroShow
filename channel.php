100d73270d2243f64d8d1532b0f079e8ba00ea76
tankwars92 <bitbybyte07@gmail.com>
2026-03-15 17:50:57 +0300

    Изменён вид страницы "Публичные видео" канала.

--- channel.php
+++ channel.php
@@ -640,6 +640,13 @@ if ($user && isset($_GET['tab']) && $_GET['tab'] === 'videos') {
                 </div>
               <?php endif; ?>
             </td>
+            <td><img src="img/pixel.gif" width="5" height="1"></td>
+          </tr>
+          <tr>
+            <td><img src="img/box_login_bl.gif" width="5" height="5"></td>
+            <td><img src="img/pixel.gif" width="1" height="5"></td>
+            <td><img src="img/box_login_br.gif" width="5" height="5"></td>
+          </tr>
         </table>
       </td>
       <td width="180">
